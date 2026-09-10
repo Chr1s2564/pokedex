a50190efcbc5ef6e0ca27c81c6841766432803d7
Chr1s2564 <chris.dli@proton.me>
2026-09-10 19:07:32 +0200

    Added: REPL logic

--- repl.go
+++ repl.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
@@ -9,4 +12,15 @@ func cleanInput(input string) []string {
 	lowered := strings.ToLower(input)
 	words = strings.Fields(lowered)
 	return words
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned := cleanInput(input)
+		fmt.Printf("Your command was: %s\n", cleaned[0])
+	}
 }
